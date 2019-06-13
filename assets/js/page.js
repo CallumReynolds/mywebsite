a9696c2bb4b7338e5b02692d9aab708cab910b92
CallumReynolds <45584439+CallumReynolds@users.noreply.github.com>
2019-06-13 14:15:15 +1000

    add updated files via upload

---
+++ assets/js/page.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+    //alert("Hello!");
+    // fade in divs
+    var fadein = $('div.fadein');
+    $.each(fadein, function(i, item) {
+        setTimeout(function() {
+            $(item).fadeIn(1000); // duration of fadein
+        }, 1000 * i); // duration between fadeins
+    });​
+// });
+
